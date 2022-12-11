f57714d64be4e9f630c33e68b9d12bf481b40e25
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2022-12-10 21:42:04 -0300

    Create calcular_areacirculo.py

---
+++ calcular_areacirculo.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+num = float(input("Digite o raio do círculo: "))
+area = 3.14*(num**2)
+print(f'A área do círculo é: {area:,.2f}')
